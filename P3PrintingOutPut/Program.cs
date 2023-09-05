6ce18e8c8cd5ea46a8e23b39e7e7dd76373dd73b
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-05 13:09:35 +0200

    Added comments and printing
    Trained on comments

--- P3PrintingOutPut/Program.cs
+++ P3PrintingOutPut/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net.Http.Json;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome, to 102 Csharp Basics");
+
+Console.WriteLine("It´s easy to print output");
+
+Console.WriteLine("... to the console");
+
--- P4Comments/Program.cs
+++ P4Comments/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+// Author:Jakob Kiryo 
+// Date: 2023-09-05
+// Email: Jakob.Kiryo@student.forsbergsskola.se
