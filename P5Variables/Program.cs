fe22ded85b660cc300f038c8eb7f723926bc806a
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-06 10:04:39 +0200

    added a new project

---
+++ P5Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("give me a number");
+
+string number = Console.ReadLine();
+
+double dbl = double.Parse(number);
+Console.WriteLine(dbl);
+
+int sgl = (int)dbl;
+Console.WriteLine(sgl);
+
+int og = Convert.ToInt32(number);
+Console.WriteLine(og);
