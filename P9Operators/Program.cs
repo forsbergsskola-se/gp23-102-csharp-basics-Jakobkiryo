2ace62716000838fe1702e5ab31bb122ba2e93c8
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-06 10:48:42 +0200

    operators

---
+++ P9Operators/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = 5 + 5; // x = 10    x:10
+int y = x + 5; // y = 15    y:15
+int z = y + y; // z = 30    z:30
+Console.WriteLine(z + z); // 60
