e6291d2d4064c85398851974f3bc11aae3098cab
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-06 12:50:12 +0200

    Update Program.cs

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//Exercise 1
+//Ask the user for an input in km/h
+//Print the result of converting  the value to m/s
 
+Console.WriteLine("Welcome to km/h to m/2 converter");
+
+Console.WriteLine("Please enter a value in km/h");
+
+string userInput = Console.ReadLine();
+
+float usernumber = float.Parse(userInput);
+
+float x = usernumber * 1000 / 60 / 60;
+
+Console.WriteLine($"That would be {x} m/s");
+
+
+//Exercise 2
+                
