2b3b85012509e3fbf67da1c356a2d0605e5c0228
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-07 12:05:30 +0200

    Booelan exercise

---
+++ Booleans/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// True or False (booleans)
+int itemcost = 500;
+
+Console.WriteLine("The item costs: [itemcosts]");
+
+Console.WriteLine("how much you got");
+
+string userInput = Console.ReadLine();
+
+int usermoney = int.Parse(userInput);
+
+Console.WriteLine("lets see");
+
+bool enough = usermoney >= itemcost;
+
+Console.WriteLine(enough);
+
+
+
+      
+
+
+
