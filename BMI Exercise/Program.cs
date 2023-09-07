94ce5901628ae016d3cb275020675690471912dc
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-07 12:15:40 +0200

    BMI Calculator

---
+++ BMI Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Make an BMI Calculator
+
+Console.WriteLine("Write your Weight and Height");
+
+string userinput = Console.ReadLine();
+double weight = double.Parse(userinput);
+
+string userinput2 = Console.ReadLine();
+double height = double.Parse(userinput2);
+
+double BMI = weight/height/height;
+
+Console.WriteLine($"Your BMI {BMI}");
+
+
+
