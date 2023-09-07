326e831bcf3a84f263dfc98ae714a9d136192602
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-07 10:07:44 +0200

    Unary minus operator

---
+++ Exercise6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices.JavaScript;
+//unary minus operator
+//how to make a negative number become postive and vice versa
+Console.WriteLine("Hello, World!");
+
+int a = -10;
+
+int b = -a -5 ; 
+
+Console.WriteLine(b);
