60f122e43ab2adc9a241a067492278b2a278e52b
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-07 09:29:48 +0200

    Made a exercise
    calculating the Area of a circle

--- Exercise 4/Program.cs
+++ Exercise 4/Program.cs
@@ -2,16 +2,18 @@
 //remainders when using divison
 Console.WriteLine("Welcome to Divison");
 
+Console.WriteLine("Please enter a Vaule in numbers");
+
 string userInput = Console.ReadLine();
 
-int usernumber = int.Parse(userInput);
+int usernumber1  = int.Parse(userInput);
 
 Console.WriteLine("Please enter a value in number");
 
 string userInput2 = Console.ReadLine();
 
-int usernumber2 = int.Parse(userInput2);
+int usernumber2  = int.Parse(userInput2);
 
-float c = (float)usernumber/ usernumber2;
+float c = (float)usernumber1 % usernumber2;
 
 Console.WriteLine($"That would be {c} ");
---
+++ Exercise5/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter Radius");
+
+double radious = Convert.ToDouble(Console.ReadLine());
+
+double area = Math.PI * radious * radious;
+
+Console.WriteLine(" Area of circle " + area);
+
+Console.ReadKey();
+
