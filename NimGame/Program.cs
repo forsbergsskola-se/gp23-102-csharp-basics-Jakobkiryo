ae25fa004d3aac8255cc6cba2f65bc58a710a67c
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-15 01:20:38 +0200

    trying to fix goto
    goto make my code unreachable and cant make code after that

--- NimGame/Program.cs
+++ NimGame/Program.cs
@@ -6,11 +6,11 @@ Console.WriteLine("Welcome to Nim");
 Console.WriteLine("|||||||||||||||||||||||| (24)");;
 
 int totalMatches = 24;
-
+matches:
     Console.WriteLine("How many matches do you want to draw?");
     int userInput = int.Parse(Console.ReadLine());
 
-matches:
+
 //Maximum matches you can pick is 3.
 if (userInput is >3 or < 1)
 {
@@ -27,13 +27,14 @@ if (userInput >= 0 && totalMatches <= 24);
     Console.WriteLine($"Player draws {userInput}");
     totalMatches -= userInput;
     
-    
 //AI:S turn
-
     int aiChoice = Random.Shared.Next(1, 4);
     Console.WriteLine($"The AI draws {aiChoice}");
     totalMatches -= aiChoice;
     Console.WriteLine("Matches Reamining:" + totalMatches);
-    
+
     goto matches;
+    
 }
+
+// win and game over
