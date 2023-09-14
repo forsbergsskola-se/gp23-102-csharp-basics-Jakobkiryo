176a44aba40cb8d268d2f127995264df14223842
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-14 13:23:08 +0200

    NimGame

---
+++ NimGame/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Welcome to Nim!");
+Console.WriteLine("|||||||||||||||||||||||| (24)");
+
+int totalmatches = 24;
+
+matches:
+
+Console.WriteLine("How many matches do you want to draw?");
+
+int userInput = int.Parse(Console.ReadLine());
+
