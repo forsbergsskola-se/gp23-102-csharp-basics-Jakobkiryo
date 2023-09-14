cc43136c2b65854957f0fc5caf05f967817655c3
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-14 12:19:51 +0200

    P8Conversion exercise

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Give me a number");
+string number = Console.ReadLine();
+
+double d = double.Parse(number);
+
+Console.WriteLine(d
+);
+int i = (int)d;
+
+Console.WriteLine(i);
+int o = int.Parse(number);
+
+Console.WriteLine(o);
