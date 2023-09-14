ad6e0656ce95d0a003f82a77e4f49d0c8600e501
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-09-15 01:06:42 +0200

    Update Program.cs

--- NimGame/Program.cs
+++ NimGame/Program.cs
@@ -1,11 +1,39 @@
-﻿Console.Write("Welcome to Nim!");
-Console.WriteLine("|||||||||||||||||||||||| (24)");
+﻿//Start of the game
 
-int totalmatches = 24;
+Gamestart:
+
+Console.WriteLine("Welcome to Nim");
+Console.WriteLine("|||||||||||||||||||||||| (24)");;
+
+int totalMatches = 24;
+
+    Console.WriteLine("How many matches do you want to draw?");
+    int userInput = int.Parse(Console.ReadLine());
 
 matches:
+//Maximum matches you can pick is 3.
+if (userInput is >3 or < 1)
+{
+    Console.WriteLine("Trying to cheat ehh, you can only pick between 1 - 3, Pick Again!");
+    goto matches;
+}
 
-Console.WriteLine("How many matches do you want to draw?");
+if (userInput >= 0 && totalMatches <= 24);
 
-int userInput = int.Parse(Console.ReadLine());
+//players turn.
 
+{
+    int userDraw = totalMatches - userInput;
+    Console.WriteLine($"Player draws {userInput}");
+    totalMatches -= userInput;
+    
+    
+//AI:S turn
+
+    int aiChoice = Random.Shared.Next(1, 4);
+    Console.WriteLine($"The AI draws {aiChoice}");
+    totalMatches -= aiChoice;
+    Console.WriteLine("Matches Reamining:" + totalMatches);
+    
+    goto matches;
+}
